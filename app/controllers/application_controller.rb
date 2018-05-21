65fe17532464615d45e18cf08af7d06b812e7d68
Mike LaRocca <michaellarocca90@gmail.com>
2018-05-21 16:52:37 -0700

    connecting front end to back end part 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  def index
+    apts=Apt.all
+    render json: apts
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+apts_attributes = [
+  {
+    id: 1,
+    name: 'Beverly Hills',
+    address1:'abc st',
+    address2:'apt b',
+    city:'SD',
+    state:'CA',
+    manager: 'James',
+    phone:123-345-6789,
+    contacthours:'5-9pm'
+  },
+  {
+    id: 2,
+    name: 'Mission Valley',
+    address1:'cde st',
+    address2:'apt c',
+    city: 'SD',
+    state: 'CA',
+    manager: 'Lemmons',
+    phone:123-444-7878,
+    contacthours:'1-7pm'
+  }
+]
+
+apts_attributes.each do |attributes|
+  Apt.create(attributes)
+end
