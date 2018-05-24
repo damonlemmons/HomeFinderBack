c44141fa6774740de80ed906260e0d9fc3a54e81
damonlemmons <jumproper22@gmail.com>
2018-05-24 16:33:51 -0700

    added apartment creation form and linked entry into database and back to frontend.

--- app/controllers/apts_controller.rb
+++ app/controllers/apts_controller.rb
@@ -4,4 +4,18 @@ class AptsController < ApplicationController
    apts=Apt.all
    render json: apts
  end
+
+ def create
+    apt = Apt.create(apt_params)
+      if apt.valid?
+        render json: apt
+      else
+        render json: apt.errors, status: :unprocessable_entity
+      end
+  end
+
+  def apt_params
+    params.require(:apt).permit(:name, :address1, :address2, :city, :state, :manager, :phone, :contacthours)
+  end
+
 end
