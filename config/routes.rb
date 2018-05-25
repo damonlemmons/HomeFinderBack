a15baf9a27c72ac4f02155095422216f0165aab8
Your Name <you@example.com>
2018-05-25 14:21:14 -0700

    bug fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users
+  
   post 'user_token' => 'user_token#create'
   resources :apts
+
+  # get '/user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
